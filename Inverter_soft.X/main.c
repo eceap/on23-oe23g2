bb757e3bc5df1a3ce985b5e19b0cb89c9ec3341b
Julian <Julian@Nora-PC>
2023-09-22 14:13:13 -0300

    Se cambio el estado "DIAGNOSTICO" por una funcion diagnosticar(); Se agregó el buzzer informante de fallas Se aplico el adc para medir fallas en el regulador Se volvio a agregar la definicion de PIN_TEC1

--- Inverter_soft.X/main.c
+++ Inverter_soft.X/main.c
@@ -7,21 +7,20 @@
 #include "tick.h"       /* Funciones/Parametros Tick */
 #include <stdio.h>      /* Archivo de Encabezados StdIO */
 
-/*==================[definiciones y macros]==================================*/
-// Nuevo tipo de datos enumerado llamado estadoMEF_t
+/*=======================[definiciones & Macros]=============================*/
 
-typedef enum {
-    INICIAL, CORRECTO, FALLAS, DIAGNOSTICO
-} estadoMEF_t;
+#define BUZZER(a) (a==1?PORTAbits.RA1=0:PORTAbits.RA1=1)
+
+typedef enum {INICIAL, CORRECTO, FALLAS, DIAGNOSTICO} estadoMEF_t;
 
 /*==================[definiciones de datos internos]=========================*/
 estadoMEF_t estadoActual; // Variable de estado (global)
 tick_t tInicio;
 //Mensajes de puerto serie
 unsigned char MSJ_FallaFuse[] = {"FalloElFusible-"};
-unsigned char MSJ_FallaCI[] = {"FalloElIntegrado-"}; //Arrays de caracteres 
-unsigned char MSJ_FallaRegulador[] = {"FalloElRegulador-"}; //Para puerto serie 
-unsigned char MSJ_FallaTrafPrim[] = {"FalloElTransformador1-"};
+unsigned char MSJ_FallaCI[] = {"FalloElIntegrado-"};             
+unsigned char MSJ_FallaRegulador[] = {"FalloElRegulador-"};     //Arrays de caracteres
+unsigned char MSJ_FallaTrafPrim[] = {"FalloElTransformador1-"}; //Para puerto serie 
 unsigned char MSJ_FallaTrafSecu[] = {"FalloElTransformador2-"};
 unsigned char MSJ_FallaGeneral[] = {"FallaGeneral-"};
 unsigned char MSJ_Normal[] = {"TodoCorrecto-"};
@@ -30,13 +29,16 @@ unsigned char MSJ_Normal[] = {"TodoCorrecto-"};
 /*==================[declaraciones de funciones internas]====================*/
 void InicializarMEF(void);
 void ActualizarMEF(void);
+uint16_t diagnosticar(void);
+void apagarLedsFallas(void);
 
-/*==================[funcion principal]======================================*/
-uint8_t Fallas;
+/*==================[Definicion de Variables]================================*/
+
 __bit FallaFuse, FallaCI, FallaRegulador, FallaTrafPrim, FallaTrafSecu, FallaGeneral; //Flags de fallas
 
+/*==================[funcion principal]======================================*/
 void main(void) {
-    /* TODO: Agregar el Cod1iogo de la Aplicación aquí */
+    
     appInit(); /* Inicializa I/O y Periféricos de la aplicación */
     InicializarMEF();
 
@@ -46,132 +48,93 @@ void main(void) {
     }
 }
 /*==================[definiciones de funciones internas]=====================*/
-// Función Inicializa MEF
+//Función Inicializa MEF
 
 void InicializarMEF(void) {
     estadoActual = INICIAL; // Establece estado inicial 
     tInicio = tickRead(); // También inicia temporizacion    
 }
 
-// Función Actualizar MEF
+//Función Actualizar MEF
 
 void ActualizarMEF(void) {
     switch (estadoActual) {
         case INICIAL:
             PIN_LED_INICIAL = 1; //Prendo LED indicador de etapa de inicializacion
-            if (!PIN_OPTO == 1) {
-                estadoActual = DIAGNOSTICO;
-                PIN_LED_INICIAL = 0; // apago el led para indicar que se termino el estado inicial
+            if (!PIN_OPTO) {
+                estadoActual = CORRECTO; //Se detecto tension a la salida del trafo, levantó correctamente el circuito
+                PIN_LED_INICIAL = 0;
             }
-            if (tickRead() - tInicio > 5000 && !PIN_OPTO == 0) {
-                estadoActual = FALLAS;
+            if ( ((tickRead() - tInicio )> 5000) && PIN_OPTO ) {
+                estadoActual = FALLAS; // No se detectó tension en la salida del trafo pasados 5 segundos
+                PIN_LED_INICIAL = 0;
             }
             break;
+            
         case CORRECTO:
-            PIN_LED_INICIAL = 0; //Apago LED indicador de etapa de inicializacion
-            PIN_LED_FALLA_CI = 0;        
-            PIN_LED_NORMAL = 0;           
-            PIN_LED_INICIAL = 0;        
-            PIN_LED_FALLA_TRAFO = 0;   
-            PIN_LED_FALLA_ZENER = 0;     
-            PIN_LED_FALLA_GENERAL = 0;
-            PIN_LED_NORMAL = 1; //Prendo LED indicador de estado de respuesta normal
+            apagarLedsFallas();         //Apago LEDs indicadores de fallas
+            PIN_LED_NORMAL = 1;         //Prendo LED indicador de estado de respuesta normal
             for (uint8_t i; i < sizeof MSJ_Normal; i++) {
                  uart2WriteByte(MSJ_Normal[i]);
+            }  
+            if(diagnosticar()== 1){
+                estadoActual = FALLAS
+                BUZZER(1);
             }
-            estadoActual = DIAGNOSTICO;
             break;
             
-            
-            
         case FALLAS:
-            PIN_LED_NORMAL = 0;
-            PIN_LED_FALLA_GENERAL = 1;
-            
-            if (FallaFuse == 1){
-                for (uint8_t i; i < sizeof MSJ_FallaFuse; i++) {
-                    uart2WriteByte(MSJ_FallaFuse[i]);
+            PIN_LED_NORMAL = 0;                 //Apago el LED indicador del estado CORRECTO
+            PIN_LED_FALLA_GENERAL = 1;          //Prendo LED indicador del estado Falla General
+            if (FallaRegulador == 1){
+                for (uint8_t i; i < sizeof MSJ_FallaRegulador; i++) {
+                    uart2WriteByte(MSJ_FallaRegulador[i]);
                 }   
                 for (uint8_t i; i < sizeof MSJ_FallaGeneral; i++) {
                     uart2WriteByte(MSJ_FallaGeneral[i]);
                 }
-            }   
+            PIN_LED_FALLA_ZENER = 1;
+            }         
+            if (FallaFuse == 1){    //El LED de esta falla se prende por Hardware
+                for (uint8_t i; i < sizeof MSJ_FallaFuse; i++) {
+                    uart2WriteByte(MSJ_FallaFuse[i]);
+                }                                                       
+                for (uint8_t i; i < sizeof MSJ_FallaGeneral; i++) {
+                    uart2WriteByte(MSJ_FallaGeneral[i]);
+                }                   //Mando mensajes informando el error
+            }               
             if (FallaCI == 1){
                 for (uint8_t i; i < sizeof MSJ_FallaCI; i++) {
                     uart2WriteByte(MSJ_FallaCI[i]);
-                }
+                }                   
                 for (uint8_t i; i < sizeof MSJ_FallaGeneral; i++) {
                     uart2WriteByte(MSJ_FallaGeneral[i]);
-                }
-            PIN_LED_FALLA_CI = 1;
-            }
+                }                   //Mando mensajes informando el error
+            PIN_LED_FALLA_CI = 1;   //Prendo LED correspondiente
+            }        
             if(FallaTrafPrim == 1){
                 for (uint8_t i; i < sizeof MSJ_FallaTrafPrim; i++) {
                     uart2WriteByte(MSJ_FallaTrafPrim[i]);
                 }
                 for (uint8_t i; i < sizeof MSJ_FallaGeneral; i++) {
                     uart2WriteByte(MSJ_FallaGeneral[i]);
-                }
+                }                   //Mando mensajes informando el error
             PIN_LED_FALLA_TRAFO = 1;
             }
+            
             if(FallaTrafSecu == 1){
                 for (uint8_t i; i < sizeof MSJ_FallaTrafSecu; i++) {
                     uart2WriteByte(MSJ_FallaTrafSecu[i]);
                 }
                 for (uint8_t i; i < sizeof MSJ_FallaGeneral; i++) {
                     uart2WriteByte(MSJ_FallaGeneral[i]);
-                }
-             PIN_LED_FALLA_TRAFO = 1;
+                }                   //Mando mensajes informando el error
+            PIN_LED_FALLA_TRAFO = 1;
             }
-            estadoActual = DIAGNOSTICO;
-            break;
-            
-            
-            
-        case DIAGNOSTICO:
-            PIN_LED_INICIAL = 0; //Apago LED indicador de etapa de inicializacion
-            if (PIN_FUSIBLE == 1) {            
-                FallaFuse = 1;
-                Fallas++;
+            if (diagnosticar() == 0){
+                estadoActual = CORRECTO;
+                BUZZER(0);
             }
-
-
-            if (PIN_QA_CI == 1) {
-                if ((tickRead() - tInicio > 11 && !PIN_QA_CI == 0)||(PIN_QA_CI == 1 && PIN_QB_CI ==1) ) {             
-                    FallaCI = 1;
-                    Fallas++;
-                }
-            }
-            if (PIN_QB_CI == 1) {
-                if (tickRead() - tInicio > 11 && !PIN_QB_CI == 0) {
-                    FallaCI = 1;
-                    Fallas++;
-                }
-            }
-
-
-            if (PIN_QA == 1) {
-                if (tickRead() - tInicio > 11 && !PIN_QA == 0) {                
-                    FallaTrafPrim = 1;
-                    Fallas++;
-                }
-            }
-            if (PIN_QB == 1) {
-                if (tickRead() - tInicio > 11 && !PIN_QB == 0) {
-                    FallaTrafPrim = 1;
-                    Fallas++;
-                }
-            }
-            
-
-            if (!PIN_OPTO == 0) {
-                FallaTrafSecu;
-                Fallas++;
-            }
-                        
-            if(Fallas = 0) {estadoActual = CORRECTO;}   
-            if(Fallas != 0){estadoActual = FALLAS;}
-
             break;
 
 
@@ -181,4 +144,56 @@ void ActualizarMEF(void) {
             // lugar seguro, por ejemplo, la reinicio:
             InicializarMEF();
     }
+}
+//Función para apagar leds
+void apagarLedsFallas (void){
+            PIN_LED_FALLA_CI = 0;       //
+            PIN_LED_INICIAL = 0;        //
+            PIN_LED_FALLA_TRAFO = 0;    //Apago LEDs indicadores de fallas
+            PIN_LED_FALLA_ZENER = 0;    //
+            PIN_LED_FALLA_GENERAL = 0;  //
+}
+//Función de diagnostico de fallas
+uint16_t diagnosticar(void) {
+    if (adcRead(PIN_ZENNER) - 2 < 733) {
+        FallaRegulador = 1;
+        return (1);
+    }
+    if (PIN_FUSIBLE == 1) {
+        FallaFuse = 1;
+        return (1);
+    }
+
+    if (PIN_QA_CI == 1) {
+        if ((tickRead() - tInicio > 11 && !PIN_QA_CI == 0) || (PIN_QA_CI == 1 && PIN_QB_CI == 1)) {
+            FallaCI = 1;
+            return (1);
+        }
+    }
+    if (PIN_QB_CI == 1) {
+        if (tickRead() - tInicio > 11 && !PIN_QB_CI == 0) {
+            FallaCI = 1;
+            return (1);
+        }
+    }
+
+    if (PIN_QA == 1) {
+        if (tickRead() - tInicio > 11 && !PIN_QA == 0) {
+                FallaTrafPrim = 1;
+                return (1);
+            }
+        }
+    if (PIN_QB == 1) {
+        if (tickRead() - tInicio > 11 && !PIN_QB == 0) {
+            FallaTrafPrim = 1;
+            return (1);
+        }
+    }
+    
+    if (!PIN_OPTO == 0) {
+        FallaTrafSecu = 1;
+        return (1);
+    }
+    if  (!PIN_TEC1 == 1){BUZZER(0);}
+    return (0);
 }
